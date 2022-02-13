5e12a60d8f1a0c76d27f9f83eb4d77b30cbb1e80
Paiva <danpaiva@live.com>
2022-02-12 22:03:35 -0300

    Adicionada configurações utilizando EF Core

---
+++ MiniTodo/Program.cs
@@ -0,0 +1,30 @@
+using MiniTodo.Data;
+using MiniTodo.ViewModels;
+using Flunt;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>();
+
+var app = builder.Build();
+
+app.MapGet("/v1/todos", (AppDbContext context) =>
+    {
+        var todos = context.Todos.ToList();
+        return Results.Ok(todos);
+    }
+);
+
+app.MapPost("/v1/todos", (
+    AppDbContext context,
+    CreateTodoViewModel model) =>
+{
+    var todo = model.MapTo();
+    if (!model.IsValid)
+        return Results.BadRequest(model.Notifications);
+    context.Todos.Add(todo);
+    context.SaveChanges();
+
+    return Results.Created($"/v1/todos/{todo.Id}", todo);
+});
+
+app.Run();
